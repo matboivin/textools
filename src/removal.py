c393827d6e45859907b3c6d0321a58fb16345f36
Mathilde BOIVIN <mboivin@student.42.fr>
2022-02-22 16:06:06 +0100

    Remove stopwords

--- src/removal.py
+++ src/removal.py
@@ -2,6 +2,8 @@
 
 import re
 import string
+import nltk
+from nltk.corpus import stopwords
 
 def remove_punctuation(text):
     text = "".join([i for i in text if i not in string.punctuation])
@@ -9,3 +11,10 @@ def remove_punctuation(text):
 
 def remove_urls(text):
     return re.sub(r'https?://\S+|www\.\S+', "", text)
+
+def remove_stopwords(language, tokens):
+    if language.lower() not in stopwords.fileids():
+        raise ValueError(f"remove_stopwords: language '{language}' is not available")
+    ignored_words = nltk.corpus.stopwords.words(language)
+    result = [token for token in tokens if token.lower() not in ignored_words]
+    return result
---
+++ tests/test_rm_stopwords.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pytest
+from src.removal import remove_stopwords
+from nltk.tokenize import word_tokenize
+
+language = "French"
+text = "Wikipédia est un projet d’encyclopédie collective en ligne, universelle, multilingue et fonctionnant sur le principe du wiki. Ce projet vise à offrir un contenu librement réutilisable, objectif et vérifiable, que chacun peut modifier et améliorer."
+tokens = word_tokenize(text, language)
+expected = ["Wikipédia", "projet", "’", "encyclopédie", "collective", "ligne", ",", "universelle", ",", "multilingue", "fonctionnant", "principe", "wiki", ".", "projet", "vise", "offrir", "contenu", "librement", "réutilisable", ",", "objectif", "vérifiable", ",", "chacun", "peut", "modifier", "améliorer", "."]
+
+def test_remove_stopwords_err():
+    with pytest.raises(ValueError):
+        result = remove_stopwords("français", tokens)
+
+def test_remove_stopwords():
+    result = remove_stopwords(language, tokens)
+    assert result == expected
